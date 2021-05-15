14d400c17276c52f65b6fd0407837e22d7c8bedc
dszenes <73389030+dszenes@users.noreply.github.com>
2021-05-15 14:48:27 +0200

    Début NLP

---
+++ NLP.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+
+df = pd.read_csv('dataframe2.csv')
+
+sia = SentimentIntensityAnalyzer()
+
+def list_sentiment(list):
+    sentiment_list = []
+    for i in range(len(list)):
+        sentiment_list.append(sia.polarity_scores(list[i]))
+    return(sentiment_list)
+
+df['Title sentiment'] = list_sentiment(df['All titles'])
+
+
+
+
+
